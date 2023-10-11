f6c79f07ccc929d197c72d96585f440d4e8c1b0a
FelixGarMe <mzpro123@gmail.com>
2023-10-10 23:03:00 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+// Ruta para renderizar la plantilla Pug
+app.get('/pug', (req, res) => {
+  res.render('Pug', { mensaje: '¡Hola desde la plantilla Pug!' });
+});
+
+// Ruta para renderizar la plantilla EJS
+app.get('/ejs', (req, res) => {
+  res.render('Ejs', { mensaje: '¡Hola desde la plantilla EJS!' });
+});
+
+
+
+app.get('/perfil/:id', (req, res) => {
+  const userId = req.params.id;
+  // Aquí puedes buscar los datos del usuario en una base de datos, por ejemplo
+  const user = { id: userId, nombre: 'Usuario ' + userId };
+  res.render('perfil', { user: user });
+});
+
+// Configurar el motor de plantillas
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// Ruta para renderizar la plantilla Pug
+app.get('/pug', (req, res) => {
+  res.render('index', { nombre: 'Usuario Pug' });
+});
+
+// Configurar EJS como motor de plantillas para una ruta específica
+app.engine('ejs', require('ejs').renderFile);
+
+// Ruta para renderizar la plantilla EJS
+app.get('/ejs', (req, res) => {
+  res.render('index.ejs', { nombre: 'Usuario EJS' });
+});
+
+// Iniciar el servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log('Aplicación web dinámica ejecutándose en el puerto 3000');
+});
